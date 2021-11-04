0b08f4c448568ec09a577441c5d63e66803102de
rhit-detterwr <37033736+detterwr@users.noreply.github.com>
2021-11-04 17:30:39 -0400

    restaurants can be stored

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,25 +7,27 @@
 var rhit = rhit || {};
 
 /** globals */
-rhit.FB_COLLECTION_RESTAURANTS = "Restaurants";
-rhit.FB_COLLECTION_USERS = "Users";
-rhit.FB_COLLECTION_FILTERS = "Filters";
-
-rhit.FB_KEY_DISCRIPTION = "discription";
+rhit.FB_COLLECTION_RESTAURANTS = "restaurants";
 rhit.FB_KEY_TITLE = "title";
-rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.FB_KEY_DESCRIPTION = "description";
 rhit.FB_KEY_LOCATION = "locationLink";
-rhit.FB_KEY_RECIPE = "recipeLink";
-
-
-rhit.FB_KEY_USERNAME = "userName";
-rhit.FB_KEY_UID = "uid";
-
+rhit.FB_KEY_MENU = "menuLink";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.FB_KEY_AUTHOR = "author"; 
 rhit.RestaurantsManager = null;
-rhit.UserManager = null;
-rhit.SingleUserManager = null;
 rhit.SingleRestaurantManager = null;
 rhit.fbAuthManager = null;
+
+
+
+// rhit.FB_COLLECTION_USERS = "Users";
+// rhit.FB_COLLECTION_FILTERS = "Filters";
+// rhit.FB_KEY_USERNAME = "userName";
+// rhit.FB_KEY_UID = "uid";
+
+// rhit.UserManager = null;
+// rhit.SingleUserManager = null;
+
 
 // from: https://stackoverflow.com/questions/3103962/converting-html-string-into-dom-elements
 function htmlToElement(html) {
@@ -35,20 +37,228 @@ function htmlToElement(html) {
 	return template.content.firstChild;
 };
 
-rhit.ClassName = class {
+rhit.ListPageController = class {
 	constructor() {
+		document.querySelector("#menuShowRestaurants").addEventListener("click", (event) => {
+			window.location.href = "/list.html"; 
+		}); 
+		document.querySelector("#menuApplyFilters").addEventListener("click", (event) => {
+			window.location.href = `/list.html?uid=${rhit.fbAuthManager.uid}`; 
+		}); 
+		document.querySelector("#menuApplyFilters").addEventListener("click", (event) => {
+			//TODO
+			console.log("applying filters"); 
+		}); 
+		document.querySelector("#menuShowFavorites").addEventListener("click", (event) => {
+			//TODO
+		}); 
+		
+		document.querySelector("#menuSignOut").addEventListener("click", (event) => {
+			rhit.fbAuthManager.signOut(); 
+		}); 
 
+
+		document.querySelector("#submitAddRestaurant").addEventListener("click", (event) => {
+			const title = document.querySelector("#inputTitle").value;
+			const menu = document.querySelector("#inputMenuLink").value; 
+			const maps = document.querySelector("#inputMapsLink").value; 
+			const description = document.querySelector("#inputDescription").value; 
+ 
+			rhit.RestaurantsManager.add(title, menu, maps, description); 
+		}); 
+
+		$('#addRestaurantDialog').on('show.bs.modal', (event) => {
+			// Pre animation
+			document.querySelector("#inputTitle").value = "";
+			document.querySelector("#inputMenuLink").value = ""; 
+			document.querySelector("#inputMapsLink").value = ""; 
+			document.querySelector("#inputDescription").value = ""; 
+		}); 
+
+		$("#addRestaurantDialog").on("shown.bs.modal", (event) => {
+			// Post animation
+			document.querySelector("#inputTitle").focus();
+		}); 
+
+		//Start listening
+		rhit.RestaurantsManager.beginListening(this.updateList.bind(this)); 
+	}
+	_createCard(restaurant) {
+		return htmlToElement(`<div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${restaurant.title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${restaurant.locationLink}</h6>
+		  <h6 class="card-subtitle mb-2 text-muted">${restaurant.menuLink}</h6>
+		  <p class="card-subtitle mb-2 text-muted">${restaurant.description}</p>
+        </div>
+      </div>`); 
 	}
 
-	methodName() {
+	updateList() {
+		console.log(" I need to update the list on the page!"); 
+		console.log(`Num Quotes: ${rhit.RestaurantsManager.getLength()}`); 
+		console.log(rhit.RestaurantsManager.getRestaurantAtIndex(0)); 
 
+		//make a new quotelistcontainer, loop to fill it, remove the old one, and put it in the new container. 
+		const newList = htmlToElement('<div id="restaurantListContainer"></div>'); 
+
+		for (let i = 0; i < rhit.RestaurantsManager.getLength(); i++) {
+			const rt = rhit.RestaurantsManager.getRestaurantAtIndex(i); 
+			const newCard = this._createCard(rt);
+
+			newCard.onclick = (event) =>{
+				console.log(`you clicked on ${rt.id}`); 
+				//rhit.storage.setMovieQuoteId(mq.id); 
+				
+				window.location.href = `/restaurant.html?id=${rt.id}`; 
+
+			};
+
+
+			newList.appendChild(newCard);  
+		}
+
+		const oldList = document.querySelector("#restaurantListContainer"); 
+		oldList.removeAttribute("id"); 
+		oldList.hidden = true;
+
+		oldList.parentElement.appendChild(newList); 
 	}
 }
+
+rhit.restaurant = class {
+	constructor(id, title, menuLink, locationLink, description){
+		this.id = id;
+		this.title = title; 
+		this.menuLink = menuLink; 
+		this.locationLink = locationLink; 
+		this.description = description; 
+	}
+}
+
+rhit.FbRestaurantsManager = class {
+	constructor(uid){
+	console.log("created FbRestaurantsManager")
+	this._uid = uid; 
+	this._documentSnapshots = [];
+	this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_RESTAURANTS);
+	this._unsubscribe = null;
+	}
+
+	add(title, menuLink, locationLink, description){
+		this._ref.add({
+			[rhit.FB_KEY_TITLE]: title, 
+			[rhit.FB_KEY_MENU]: menuLink,
+			[rhit.FB_KEY_LOCATION]: locationLink,
+			[rhit.FB_KEY_DESCRIPTION]: description,
+			//[rhit.FB_KEY_AUTHOR]: rhit.fbAuthManager.uid,
+			[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(), 
+			
+		})
+		.then(function (docRef){
+			console.log("document written with id: ", docRef.id);
+		})
+		.catch(function (error) {
+			console.error("error adding document: ", error); 
+		});
+	}
+
+	beginListening(changeListener) {
+		//let query = this._ref.orderBy(rhit.FB_KEY_LAST_TOUCHED, "desc").limit(50);
+		let query = this._ref;
+		// if (this._uid) {
+		// 		query = query.where(rhit.FB_KEY_AUTHOR, "==", this._uid);
+		// }
+		this._unsubscribe = query.onSnapshot((querySnapshot) => {
+				console.log("Restaurant update!");
+				this._documentSnapshots = querySnapshot.docs;
+
+				changeListener();
+			});
+	}
+	stopListening() {
+		this._unsubscribe(); 
+	}
+
+	getLength() {
+		return this._documentSnapshots.length;
+	}
+	getRestaurantAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index]; 
+		const rt = new rhit.restaurant(
+			docSnapshot.id,
+			docSnapshot.get(rhit.FB_KEY_TITLE),
+			docSnapshot.get(rhit.FB_KEY_MENU),
+			docSnapshot.get(rhit.FB_KEY_LOCATION),
+			docSnapshot.get(rhit.FB_KEY_DESCRIPTION)
+		);
+		return rt;
+	}
+}
+
+// PUT THE DETAIL PAGE CONTROLLER HERE 
+
+// PUT THE SINGLE RESTAURANT MANAGER HERE
+
+// PUT THE LOGIN PAGE CONTROLLER HERE
+
+// PUT THE AUTH MANAGER HERE 
+
+rhit.checkForRedirects = function(){
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn){
+		window.location.href = "/list.html"; 
+	}
+
+	if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn){
+		window.location.href = "/"; 
+	}
+
+};
+
+rhit.initializePage = function(){
+	const urlParams = new URLSearchParams(window.location.search); 
+	if(document.querySelector("#listPage")){
+		console.log("You are on the list page.")
+		const uid = urlParams.get("uid");
+		rhit.RestaurantsManager = new rhit.FbRestaurantsManager(uid);
+		new rhit.ListPageController();
+	}
+
+/* 	if(document.querySelector("#detailPage")){
+		console.log("You are on the detail page.")
+		const restaurantId = urlParams.get("id"); 
+
+		console.log(restaurantId); 
+		if (!restaurantId){
+			console.log("Error! Missing restaurant ID"); 
+			window.location.href = "/"; 
+		}
+		rhit.fbSingleQuoteManager = new rhit.FbSingleQuoteManager(restaurantId);
+		new rhit.DetailPageController(); 
+	
+	}
+
+	if(document.querySelector("#loginPage")){
+		console.log("You are on the login page.")
+		new rhit.LoginPageController();
+	} */
+} 
+
+
+
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
-	console.log("Ready");
+	console.log("Ready: ");
+	// rhit.fbAuthManager = new rhit.FbAuthManager();
+	// rhit.fbAuthManager.beginListening(() => {
+	// 	console.log("auth change callback fired"); 
+	// 	console.log("isSignedIn =", rhit.fbAuthManager.isSignedIn);
+	// 	rhit.checkForRedirects();
+	 	rhit.initializePage();
+		 
+	// });
 };
 
 rhit.main();
